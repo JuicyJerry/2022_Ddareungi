2500340669ff80dd94d42149330f26578760d644
이정환 사원 <junghwan.lee@dkbmc.com>
2022-03-22 11:31:12 +0900

    fix: getTypeError & try: fetch data using api

--- Dareungi/force-app/main/default/aura/Ddareungi/DdareungiHelper.js
+++ Dareungi/force-app/main/default/aura/Ddareungi/DdareungiHelper.js
@@ -1,26 +1,32 @@
 ({
     getResponse : function(component, apiKey) {
-        console.log('check point1');
         let action = component.get("c.getCalloutResponseData");
-        
-        console.log('check point2');
+    
         action.setParams({
-            "url": 'http://openapi.seoul.go.kr:8088/' + apiKey + '/json/bikeList/1/5/',
+            "url": "https://api.sampleapis.com/coffee/hot",
+            // 'url': 'http://openapi.seoul.go.kr:8088/sample/json/bikeList/1/5/',
+            // "url": `http://openapi.seoul.go.kr:8088/${apiKey}/json/bikeList/1/5/`,
         });
 
         action.setCallback(this, function(response) {
             let state = response.getState();
-            if (component.isValid() && state === "SUCCESS") {
-                component.set("v.response", response.getReturnValue());
+            console.log(component.isValid(), state);
 
+            if (component.isValid() && state === "SUCCESS") {
+                console.log('success');
+                
+                component.set("v.response", response.getReturnValue());
+                
                 let getAllRates = component.get("v.response")['rates'];
                 let DataList = [];
-
+                
                 for (let key in getAllRates) {
                     DataList.push(key + ' = ' + getAllRates[key]);
                 }
-
+                
                 component.set("v.ListOfCurrency", DataList);
+            } else {
+                console.log("failure");
             }
         });
 
