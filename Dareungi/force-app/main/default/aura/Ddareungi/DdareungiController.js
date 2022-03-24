7de68c96e7f817e1e2f34666dd0b43df084ed684
이정환 사원 <junghwan.lee@dkbmc.com>
2022-03-24 18:01:24 +0900

    [display] adjust view & [feat] add search bar

--- Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
+++ Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
@@ -5,9 +5,13 @@
         let apiKey = '4251526e676c6a683639437376564c';
         
         helper.getResponse(component, apiKey);
-    }
-})
-({
+    },
+
+    handleChange: function(component, event, helper) {
+        // onchange 값 받아오기!!!!!
+        console.log(event.target.value);
+    },
+
     handleClickBtn: function(component, event, helper) {
         alert("click!")
     }    
