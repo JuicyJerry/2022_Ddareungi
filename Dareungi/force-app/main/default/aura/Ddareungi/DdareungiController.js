55a2d2c4a5e33850dededb741f20b249c9c13ff3
이정환 사원 <junghwan.lee@dkbmc.com>
2022-03-25 11:32:33 +0900

    [feat] search bar: add onchange handler & [display] show search keyword on ui properly (-ing)

--- Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
+++ Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
@@ -1,6 +1,9 @@
 ({
     handleClick : function(component, event, helper) {
+        let textValue = component.get("{!v.textValue}");
+        alert(`"${textValue}" 을 검색 중입니다.`);
         console.log("Get data button is clicked");
+        // alert("click!")
 
         let apiKey = '4251526e676c6a683639437376564c';
         
@@ -9,10 +12,21 @@
 
     handleChange: function(component, event, helper) {
         // onchange 값 받아오기!!!!!
-        console.log(event.target.value);
+        let findValue = component.find("enter-search").get("v.value");
+        console.log(findValue);
+        
+        // let listValue = [];
+        // listValue.reduce((acc, cur) => {
+        //     console.log(cur.get("v"));
+        // })
+        
+        component.set("v.textValue", findValue);
+        //  component.get("v.textvalue");
+        // console.log("textValue: " + textValue);
     },
-
-    handleClickBtn: function(component, event, helper) {
-        alert("click!")
+    
+    handleFocus: function(component, event, helper) {
+        console.log("Input has recieved the focus");
+        component.set("v.textValue", null);
     }    
 })
--- Dareungi/force-app/main/default/aura/Ddareungi/DdareungiHelper.js
+++ Dareungi/force-app/main/default/aura/Ddareungi/DdareungiHelper.js
@@ -1,7 +1,8 @@
 ({
     getResponse : function(component, apiKey) {
         let action = component.get("c.getCalloutResponseData");
-    
+        let keyword = component.get("v.textValue"); 
+
         action.setParams({
             // "url": "https://api.sampleapis.com/coffee/hot",
             // 'url': 'http://openapi.seoul.go.kr:8088/sample/json/bikeList/1/5/',
@@ -14,21 +15,32 @@
             console.log("state: " + state);
             
             if (component.isValid() && state === "SUCCESS") {
-                // console.log("success! :  " + response.getReturnValue()["rentBikeStatus"]["row"]);
-                
-                let returnValues = [];
-                let stationName = [];
+                // let returnValues = [];
+                let findKeywordData = [];
                 let rawData = ((JSON.parse([response.getReturnValue()])["rentBikeStatus"]["row"]));
 
                 for (let target of rawData) {
                     // console.log("target: " + JSON.stringify(JSON.parse(target)["rentBikeStatus"]["row"]) + '\n\n\n\n\n\n\n\n' + cnt);
-                    // console.log(target);
-                    returnValues.push(JSON.stringify(target));
-                    stationName.push((target["stationName"]));
-                    // rawData["stationName"]
+                    // returnValues.push(JSON.stringify(targ    et));
+                    // const regex = /`${keyword}`;
+                    // const regex = /`${keyword}`/g;
+                    const getKeyword = target["stationName"].match(keyword);
+                    console.log(target["stationName"], getKeyword, typeof getKeyword)
+
+                    if (typeof getKeyword === "object") {
+                        console.log("해당하는 키워드 발견");
+                        findKeywordData.push((target["stationName"]));
+                    } else if (typeof getKeyword !== "string") {
+                        console.log("string type 이 아닙니다");
+                    } else {
+                        console.log("이것 어떤 경우일까요?");
+                        console.log("검색 결과가 없습니다.");
+                        component.set("v.ListOfStations", "검색 결과가 없습니다.");
+                    }
                 }
-                component.set("v.ListOfData", (returnValues));
-                component.set("v.ListOfStations", stationName);
+                // component.set("v.ListOfData", (returnValues));
+                // component.set("v.ListOfData", findKeywordData);
+                component.set("v.ListOfStations", findKeywordData);
                 
                 // let DataList = [];
                 // let getAllData = component.get("v.ListOfData");
