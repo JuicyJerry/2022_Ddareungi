da580e58c934c70b9e753b16b38a9241f99eff8e
이정환 사원 <junghwan.lee@dkbmc.com>
2022-03-29 13:03:26 +0900

    [aura] Ddareungi Location Search Service is finished

--- Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
+++ Dareungi/force-app/main/default/aura/Ddareungi/DdareungiController.js
@@ -1,56 +1,24 @@
 ({
     handleClick : function(component, event, helper) {
         let textValue = component.get("{!v.textValue}");
-        console.log(`${textValue} data is coming!`);
-        // alert("click!")
-        // alert(`"${textValue}" 을 검색 중입니다.`);
-
         let apiKey = '4251526e676c6a683639437376564c';
         helper.getResponse(component, apiKey);
     },
-
+    
     handleChange: function(component, event, helper) {
-        // onchange 값 받아오기!!!!!
         let findValue = component.find("enter-search").get("v.value");
-        console.log(findValue);
-        
-        // let listValue = [];
-        // listValue.reduce((acc, cur) => {
-        //     console.log(cur.get("v"));
-        // })
-        
         component.set("v.textValue", findValue);
-        //  component.get("v.textvalue");
-        // console.log("textValue: " + textValue);
     },
     
     handleFocus: function(component, event, helper) {
-        console.log("Input has recieved the focus");
         component.set("v.textValue", null);
     },
 
-    // function automatic called by aura:waiting event  
     showSpinner: function(component, event, helper) {
-        // make Spinner attribute true for displaying loading spinner 
         component.set("v.spinner", true); 
     },
         
-    // function automatic called by aura:doneWaiting event 
     hideSpinner : function(component,event,helper){
-        // make Spinner attribute to false for hiding loading spinner    
         component.set("v.spinner", false);
     }
-    // showSpinner: function(component, event, helper) {
-    //     component.set("v.spinner", false);
-
-    //     setTimeout(() => {
-    //         console.log("counting!");
-    //     }, 2000)
-        
-    //     this.hideSpinner();
-    // },
-
-    // hideSpinner: function(component, event, helper) {
-    //     component.set("v.spinner", true);
-    // }
 })
